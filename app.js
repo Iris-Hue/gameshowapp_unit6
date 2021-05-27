844de41fcb66c74f4142c33a5aa40f4c3423f8ef
Iris-Hue <78930747+Iris-Hue@users.noreply.github.com>
2021-05-27 10:20:49 -0700

    add needed variable

---
+++ app.js
@@ -0,0 +1,9 @@
+/*/////////////////////////////////////
+        Added needed variables
+////////////////////////////////////*/
+
+const keyboard = document.getElementById('qwerty')
+const phrase = document.getElementById('phrase')
+const startgame = document.getElementsByClassName('btn__reset')
+
+var missed = [0];
