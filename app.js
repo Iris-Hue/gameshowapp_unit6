598fc332be80b719324d29dab8ba60ab1063c9b2
Iris-Hue <78930747+Iris-Hue@users.noreply.github.com>
2021-05-27 15:57:25 -0700

    add event listener

--- app.js
+++ app.js
@@ -5,5 +5,26 @@
 const keyboard = document.getElementById('qwerty')
 const phrase = document.getElementById('phrase')
 const startgame = document.getElementsByClassName('btn__reset')
+const overlay = document.getElementById('overlay');
+var missed = 0; //hide screen overlay
 
-var missed = [0];
+/*/////////////////////////////////
+    create array of name of pharses
+////////////////////////////////*/
+
+let pharse =
+    ['big hearted',
+    'life grows on',
+    'avid gamer',
+    'men in black',
+    'island hopping'];
+
+
+/*////////////////////////////////
+        event handlers
+/////////////////////////////////*/
+
+
+overlay.addEventListener('click',() = > {
+    if(overlay.style.display = ='none'){
+});
